536d6d4393a69b6fc3e0aefdc85b14d4b2a64799
superartyom <artyom.s.yakovlev@gmail.com>
2022-04-06 00:21:04 +0400

    input added

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import streamlit as st
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+
 
 header = st.container()
+tinput = st.container()
 
 with header:
     st.title("Welcome to ArmSpellCheck")
     st.text("Here you can check your mistakes...")
+
+model = T5ForConditionalGeneration.from_pretrained('/final-model').cuda()
+tokenizer = AutoTokenizer.from_pretrained('/final-model')
+
+with tinput:
+    inpt = st.text_input("Enter your text...", )
+    inputs = tokenizer([inpt], padding="longest", return_tensors="pt").input_ids.cuda()
+    res = model.generate(inputs)
+    st.write(tokenizer.decode(res[0]))
