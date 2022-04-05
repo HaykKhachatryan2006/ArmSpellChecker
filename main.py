07f64e714ced06e5674bba024470918e6b522879
superartyom <artyom.s.yakovlev@gmail.com>
2022-04-06 00:53:01 +0400

    input added

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from transformers import T5ForConditionalGeneration, AutoTokenizer
-
+# from transformers import T5ForConditionalGeneration, AutoTokenizer
 
 header = st.container()
 tinput = st.container()
@@ -9,11 +8,15 @@ with header:
     st.title("Welcome to ArmSpellCheck")
     st.text("Here you can check your mistakes...")
 
-model = T5ForConditionalGeneration.from_pretrained('/final-model').cuda()
-tokenizer = AutoTokenizer.from_pretrained('/final-model')
+# model = T5ForConditionalGeneration.from_pretrained('C:/Users/Lenovo/Desktop/ArmSpellcheck/final-model',).cuda()
+# tokenizer = AutoTokenizer.from_pretrained('C:/Users/Lenovo/Desktop/ArmSpellcheck/final-model')
+# inputs = tokenizer(['այծակ'], padding="longest", return_tensors="pt").input_ids.cuda()
+# res = model.generate(inputs)
+# print(res[0])
 
 with tinput:
     inpt = st.text_input("Enter your text...", )
-    inputs = tokenizer([inpt], padding="longest", return_tensors="pt").input_ids.cuda()
-    res = model.generate(inputs)
-    st.write(tokenizer.decode(res[0]))
+    # inputs = tokenizer([inpt], padding="longest", return_tensors="pt").input_ids.cuda()
+    # res = model.generate(inputs)
+    # st.write(tokenizer.decode(res[0]))
+    st.write(inpt)
