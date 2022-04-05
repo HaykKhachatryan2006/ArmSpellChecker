98c8482f1442fa62567c3e0d8d898cdcec5f1722
superartyom <artyom.s.yakovlev@gmail.com>
2022-04-05 21:31:46 +0400

    start

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import streamlit as st
 header = st.container()
 
 with header:
-    st.title("Welcome to Armspellcheck");
+    st.title("Welcome to ArmSpellCheck")
     st.text("Here you can check your mistakes...")
