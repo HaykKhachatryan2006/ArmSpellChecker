8c26160f81ffae7283d6576927c8c2488516506e
superartyom <62669220+superartyom@users.noreply.github.com>
2022-05-01 17:35:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -68,5 +68,4 @@ inputt = st.text_input(label="", value="Ձեր տեքստը")
 text = inputt.split(" ")
 with toutput:
     for i in text:
-#         st.write(t.decode(output(i, m, t)))
-        st.write(i)
+        st.write(t.decode(output(i, m, t)))
