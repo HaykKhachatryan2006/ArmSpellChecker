350fd5f7a8068bcb4620fcf990c0770b897312e4
superartyom <62669220+superartyom@users.noreply.github.com>
2022-05-16 00:51:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title='Armspellcheck')
 
